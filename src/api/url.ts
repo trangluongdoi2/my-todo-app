ba2fa19d2faffef3b8d8ec917ff641050a367cc6
Vinh Nguyen <vinhnguyentan99@gmail.com>
2024-07-28 22:25:39 +0700

    feat: config server url

--- src/api/url.ts
+++ src/api/url.ts
@@ -1,2 +1,3 @@
-// export const API_URL = 'http://localhost:9999';
-export const API_URL = 'http://localhost:9998';
+export const LOCAL_SERVER_URL = 'http://localhost:9998';
+
+export const API_URL = process.env.NODE_ENV === 'production' ? 'https://my-todo-backend-fpuu.onrender.com' : LOCAL_SERVER_URL;
