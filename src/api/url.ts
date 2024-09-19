45ca74995864a694653ca84f52fd92d3ec698d4e
Vinh Nguyen <vinhnguyentan99@gmail.com>
2024-09-19 15:14:45 +0700

    feat: update host

--- src/api/url.ts
+++ src/api/url.ts
@@ -1,3 +1,4 @@
 export const LOCAL_SERVER_URL = 'http://localhost:9998';
+export const PROD_SERVER_URL = 'http://18.138.35.101';
 
-export const API_URL = process.env.NODE_ENV === 'production' ? 'https://my-todo-backend-fpuu.onrender.com' : LOCAL_SERVER_URL;
+export const API_URL = process.env.NODE_ENV === 'production' ? PROD_SERVER_URL : LOCAL_SERVER_URL;
