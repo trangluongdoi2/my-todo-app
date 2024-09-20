aad0de0c8cf84d6c88c5e6566bd52ba0f6dd2d77
Vinh Nguyen <vinhnguyentan99@gmail.com>
2024-09-20 17:57:29 +0700

    chore: change backend api url

--- src/api/url.ts
+++ src/api/url.ts
@@ -1,4 +1,4 @@
 export const LOCAL_SERVER_URL = 'http://localhost:1000';
-export const PROD_SERVER_URL = 'http://18.138.35.101';
+export const PROD_SERVER_URL = 'https://www.server.trangluongdoi2.com';
 
 export const API_URL = process.env.NODE_ENV === 'production' ? PROD_SERVER_URL : LOCAL_SERVER_URL;
